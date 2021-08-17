fa958b6b1ac95bad411a34caf39f4887b1bac66f
VASANTHAKUMAR B <vasanthakumar.b.17@mountblue.tech>
2021-08-17 08:28:19 +0530

    Correction in mapObject function

--- mapObject.js
+++ mapObject.js
@@ -1,10 +1,9 @@
 var _ = require('underscore');
-const obj = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
-mapObject(obj);
+
 function mapObject(obj){
 
     let getMapObject = _.mapObject(obj,function(val, key) {
-  return val + 5});
+  return val + 10});
     return getMapObject;
 }
 
